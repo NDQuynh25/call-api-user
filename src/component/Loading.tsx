12f979b72f527ffc8829f93e2642918a6ae390fd
Nguyễn Đức Quỳnh <quynhnd.b21cn646@stu.ptit.edu.vn>
2024-08-01 05:43:32 +0700

    okok

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import HashLoader from 'react-spinners/HashLoader';
 
 const Loading = () => {
-  // Cài đặt kiểu cho lớp phủ mờ và loader
+  
   const overlayStyle: React.CSSProperties = {
     position: 'fixed',
     top: 0,
     left: 0,
     width: '100%',
     height: '100%',
-    background: 'rgba(255, 255, 255, 0)', // Nền trắng mờ
+    background: 'rgba(255, 255, 255, 0)', 
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    zIndex: 0, // Đảm bảo lớp phủ nằm trên tất cả các phần tử khác
+    zIndex: 0, 
   };
 
   const loaderStyle: React.CSSProperties = {
--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,15 +1,16 @@
-// axiosConfig.ts
+
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://randomuser.me/api/',
-  timeout: 10000, // Thời gian timeout
+  baseURL: 'https://randomuser.me/api/'
+  , 
+  timeout: 10000, 
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Xử lý lỗi toàn cục
+
 axiosInstance.interceptors.response.use(
   response => response,
   error => {
